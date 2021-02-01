2b6e842c015630e2a8724ea2a22667769727635e
Ryan Callahan <RyanCallahan312@gmail.com>
2021-01-31 20:32:56 -0500

    make api request and parse json to struct

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
-module test
+module Project1
 
 go 1.15
-
-require github.com/google/uuid v1.0.0
--- main.go
+++ main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"strconv"
+	"log"
+	"net/http"
 )
 
 func main() {
 
-	fmt.Print("Enter how many uuid's you would like to generate: ")
-	var input string
-	fmt.Scanln(&input)
+	httpClient := &http.Client{}
+	request, _ := http.NewRequest(http.MethodGet, "https://api.data.gov/ed/collegescorecard/v1/schools.json?page=0&school.degrees_awarded.predominant=2,3&fields=id,school.name,school.city,2018.student.size,2017.student.size,2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,2016.repayment.3_yr_repayment.overall&api_key=HK4nxy96BDiKgcWABCBVMqdctCnTWWpUcUwSbjjE", nil)
 
-	numberOfUuids, err := strconv.ParseInt(input, 10, 32)
+	resp, err := httpClient.Do(request)
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 
-	uuids := getUuids(int(numberOfUuids))
+	var parsedResponse CollegeScoreCardResponseDTO
+	json.NewDecoder(resp.Body).Decode(&parsedResponse)
 
-	for i, value := range uuids {
-		fmt.Printf("%d - %s\n", i+1, value)
-	}
+	fmt.Println(parsedResponse.TextOutput())
 }
