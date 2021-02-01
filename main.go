df74296f0f3a9245fd680239f7160ea0efc54750
Ryan Callahan <RyanCallahan312@gmail.com>
2021-01-31 23:33:22 -0500

    regenerate api key and build url and request in a more reasonable way
    regenerate api key and build url and request in a more reasonable way

--- main.go
+++ main.go
@@ -5,12 +5,48 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
 
 	httpClient := &http.Client{}
-	request, _ := http.NewRequest(http.MethodGet, "https://api.data.gov/ed/collegescorecard/v1/schools.json?page=0&school.degrees_awarded.predominant=2,3&fields=id,school.name,school.city,2018.student.size,2017.student.size,2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,2016.repayment.3_yr_repayment.overall&api_key=HK4nxy96BDiKgcWABCBVMqdctCnTWWpUcUwSbjjE", nil)
+	baseURL := "https://api.data.gov/ed/collegescorecard/v1/schools.json"
+
+	filters := make(map[string]string)
+	filters["page"] = "0"
+	filters["school.degrees_awarded.predominant"] = "2,3"
+	filters["fields"] = "id,school.name,school.city,2018.student.size,2017.student.size,2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,2016.repayment.3_yr_repayment.overall"
+	filters["api_key"] = os.Getenv("API_KEY")
+
+	requestURL := getRequestURL(baseURL, filters)
+
+	response := requestData(requestURL, httpClient)
+
+	fmt.Println(response.TextOutput())
+}
+
+func getRequestURL(baseURL string, filters map[string]string) *url.URL {
+	requestURL, err := url.Parse(baseURL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	query := requestURL.Query()
+	for key, value := range filters {
+		query.Set(key, value)
+	}
+	requestURL.RawQuery = query.Encode()
+
+	return requestURL
+}
+
+func requestData(url *url.URL, httpClient *http.Client) CollegeScoreCardResponseDTO {
+	request, _ := http.NewRequest(http.MethodGet, url.String(), nil)
 
 	resp, err := httpClient.Do(request)
 	if err != nil {
@@ -20,5 +56,5 @@ func main() {
 	var parsedResponse CollegeScoreCardResponseDTO
 	json.NewDecoder(resp.Body).Decode(&parsedResponse)
 
-	fmt.Println(parsedResponse.TextOutput())
+	return parsedResponse
 }
