c8819c807b4feb0a62180b986bdcdbd3b54ffdf3
Ryan Callahan <RyanCallahan312@gmail.com>
2021-02-03 00:49:26 -0500

    Revert "Dev to master (#18)"
    This reverts commit 1c0f02c8437fd146a508710f800c40e49c77afc9.

--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"bufio"
 	"encoding/json"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"strconv"
-	"sync"
 
 	"github.com/joho/godotenv"
 )
@@ -33,84 +31,27 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-
 	writer := bufio.NewWriter(outFile)
-	var fileLock sync.Mutex
 
-	orderChannel := make(chan int, 1)
-	orderChannel <- 0
-	response := writeRequestToFile(baseURL, filters, page, httpClient, writer, &fileLock, orderChannel)
+	response := makeRequest(baseURL, filters, httpClient)
 
-	wg := sync.WaitGroup{}
-	for (page)*response.Metadata.ResultsPerPage < response.Metadata.TotalResults {
+	for (page * response.Metadata.ResultsPerPage) < (response.Metadata.TotalResults) {
 		page++
-
-		wg.Add(1)
-		go func(page int) {
-			defer wg.Done()
-
-			flt := make(map[string]string)
-			for k, v := range filters {
-				flt[k] = v
-			}
-			flt["page"] = strconv.Itoa(page)
-
-			_ = writeRequestToFile(baseURL, flt, page, httpClient, writer, &fileLock, orderChannel)
-
-		}(page)
+		filters["page"] = strconv.Itoa(page)
+		_, err = writer.WriteString(makeRequest(baseURL, filters, httpClient).TextOutput())
+		if err != nil {
+			log.Fatalln(err)
+		}
 
 	}
 
-	wg.Wait()
-
 }
 
-func shouldWrite(requestNumber int, ch chan int) bool {
-	message := <-ch
+func makeRequest(baseURL string, filters map[string]string, httpClient *http.Client) CollegeScoreCardResponseDTO {
 
-	if requestNumber != message {
-		ch <- message
-	}
-
-	return requestNumber == message
-}
-
-func writeRequestToFile(baseURL string, filters map[string]string, page int, httpClient *http.Client, writer *bufio.Writer, fileLock *sync.Mutex, orderChannel chan int) CollegeScoreCardResponseDTO {
 	requestURL := getRequestURL(baseURL, filters)
 
-	retry := true
-	retryCount := 0
-
-	var response CollegeScoreCardResponseDTO
-	var rawResponse string
-
-	for retry {
-		response, rawResponse = requestData(requestURL, httpClient)
-		if rawResponse == "" || retryCount > 3 {
-			retry = false
-		}
-		retryCount++
-	}
-
-	for !shouldWrite(page, orderChannel) {
-
-	}
-
-	fileLock.Lock()
-	if retry {
-		_, err := writer.WriteString(rawResponse)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-	} else {
-		_, err := writer.WriteString(response.TextOutput())
-		if err != nil {
-			log.Fatalln(err)
-		}
-	}
-	fileLock.Unlock()
-	orderChannel <- page + 1
+	response := requestData(requestURL, httpClient)
 
 	return response
 }
@@ -130,7 +71,7 @@ func getRequestURL(baseURL string, filters map[string]string) *url.URL {
 	return requestURL
 }
 
-func requestData(url *url.URL, httpClient *http.Client) (CollegeScoreCardResponseDTO, string) {
+func requestData(url *url.URL, httpClient *http.Client) CollegeScoreCardResponseDTO {
 	request, _ := http.NewRequest(http.MethodGet, url.String(), nil)
 
 	resp, err := httpClient.Do(request)
@@ -140,24 +81,10 @@ func requestData(url *url.URL, httpClient *http.Client) (CollegeScoreCardRespons
 	defer resp.Body.Close()
 
 	var parsedResponse CollegeScoreCardResponseDTO
-	rawResponse := ""
-
-	if resp.StatusCode == http.StatusOK {
-
-		err = json.NewDecoder(resp.Body).Decode(&parsedResponse)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-	} else {
-
-		bodyBytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		rawResponse = string(bodyBytes)
+	err = json.NewDecoder(resp.Body).Decode(&parsedResponse)
+	if err != nil {
+		log.Fatalln(err)
 	}
 
-	return parsedResponse, rawResponse
+	return parsedResponse
 }
