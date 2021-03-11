8fbca51d3885aa9913d7837704346cdba81696dc
Ryan Callahan <RyanCallahan312@gmail.com>
2021-03-11 14:18:27 -0500

    make linter happy

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 		}
 	}
 
-	initalizeDB()
+	InitalizeDB()
 	conn, err := sql.Open("pgx", os.Getenv("WORKING_CONNECTION_STRING"))
 
 	if err != nil {
@@ -214,35 +214,6 @@ func createFilterBase() map[string]string {
 	filters["api_key"] = os.Getenv("API_KEY")
 
 	return filters
-}
-
-func initalizeDB() {
-	conn, err := sql.Open("pgx", os.Getenv("MAINTENANCE_CONNECTION_STRING"))
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	defer func() {
-
-		err := conn.Close()
-		if err != nil {
-			log.Fatalln(err)
-		}
-	}()
-
-	var dbExists bool
-	_ = conn.QueryRow(`SELECT EXISTS (
-			SELECT FROM pg_database 
-			WHERE datname = 'comp490project1'
-			)`).Scan(&dbExists)
-
-	if !dbExists {
-		_, err = conn.Exec(`CREATE DATABASE comp490project1`)
-		if err != nil {
-			log.Fatalln(err)
-		}
-	}
-
 }
 
 func initalizeTables(conn *sql.DB) {
