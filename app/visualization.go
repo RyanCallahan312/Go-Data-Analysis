4496ff15c8fe72283feba3258c45a2e9ebc6a19f
Ryan Callahan <RyanCallahan312@gmail.com>
2021-03-16 02:57:00 -0400

    add buttons for map and text visualization

--- app/visualization.go
+++ app/visualization.go
@@ -7,5 +7,9 @@ type visualization struct {
 }
 
 func (h *visualization) Render() app.UI {
-	return app.Div().Body(app.A().Text("To Homepage").Href("/"), app.H1().Text("Visualization"))
+	return app.Div().Body(app.A().Text("To Homepage").Href("/"),
+		app.H1().Text("Visualization"),
+		app.Button().Text("Text Viz"),
+		app.Button().Text("Map Viz"),
+	)
 }
